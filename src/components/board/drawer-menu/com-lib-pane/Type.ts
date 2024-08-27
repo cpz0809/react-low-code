b7a3854a3ee79ccdc0f339550ab31e503eb45242
czp <czp0809@163.com>
2024-08-27 15:45:06 +0800

    feat:新增Button组件

--- src/components/board/drawer-menu/com-lib-pane/Type.ts
+++ src/components/board/drawer-menu/com-lib-pane/Type.ts
@@ -10,7 +10,8 @@ export enum PaneItemTypes {
   Modal = 'Modal',
   Drawer = 'Drawer',
   Tabs = 'Tabs',
-  Box = 'Box'
+  Box = 'Box',
+  Button = 'Button'
 }
 
 export enum CategoryEnum {
@@ -32,17 +33,34 @@ export type PaneItemEditKey = 'name' | 'style' | 'attr' | 'hidden'
 export type PaneItemType = {
   [key in PaneItemEditKey]: any
 } & {
+  // id
   uuid: string
+  // 名称
   name: string
+  // icon
   svg: string
+  // 类型
   type: string
+  // 样式
   style: React.CSSProperties
+  // 子组件
   children: PaneItemType[]
+  // 父元素id
   parentUuid: string | null
+  // 操作类型
   operate: HistoryEnum.ADD | HistoryEnum.EDIT | null
-  index: number
+  // 可编辑属性tab标签
   editableType: EditableTypeItem[]
+  // 属性
   attr: { [key: string]: any }
+  // 是否显示
   hidden: boolean
+  // 元素类型
   categoryType: CategoryEnum
+  // 附加组件
+  // additional?: AdditionalType
 }
+
+type AdditionalType = {
+  [key in string]: PaneItemType
+} & Partial<Record<PaneItemTypes, PaneItemType>>
---
+++ src/components/compt/button/Button.tsx
@@ -0,0 +1,12 @@
+import { PaneItemType } from '@/components/board/drawer-menu/com-lib-pane/Type'
+import DraggableView from '@/components/board/drop/DraggableView'
+import { Button as AntButton } from 'antd'
+const Button = ({ item }: { item: PaneItemType }) => {
+  return (
+    <DraggableView item={item}>
+      <AntButton {...item.attr}>{item.attr.children}</AntButton>
+    </DraggableView>
+  )
+}
+
+export default Button
